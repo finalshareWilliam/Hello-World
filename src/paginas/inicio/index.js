09f1610d9e0ad12687a0bc023dfb9000e2e02b05
William Souza <william.h.souza.com>
2023-06-12 17:16:23 -0300

    v1.4

--- src/paginas/inicio/index.js
+++ src/paginas/inicio/index.js
@@ -1,13 +1,13 @@
 import styles from "./Inicio.module.css";
 import posts from "json/posts.json";
-import Post from "componentes/post";
+import PostCard from "componentes/postCard";
 
 export default function Inicio() {
     return (
         <ul className={styles.posts}>
             {posts.map((post) => (
                 <li key={post.id}>
-                    <Post post={post} />
+                    <PostCard post={post} />
                 </li>
             ))}
         </ul>
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ import SobreMim from "./paginas/sobremim";
 import Menu from "./componentes/menu";
 import Rodape from "componentes/rodape";
 import PaginaPadrao from "componentes/paginaPadrao";
+import Post from "paginas/post";
+import NaoEncontrada from "paginas/naoencontrado";
 
 function AppRoutes() {
   return (
@@ -13,10 +15,11 @@ function AppRoutes() {
       <Routes>
         <Route path="/" element={<PaginaPadrao />}>
           <Route index element={<Inicio />} />
-          <Route path="/sobremim" element={<SobreMim />} />
+          <Route path="sobremim" element={<SobreMim />} />
+          <Route path="posts/:id" element={<Post />} />
         </Route>
 
-        <Route path="*" element={<div>Error 404</div>} />
+        <Route path="*" element={<NaoEncontrada/>} />
       </Routes>
       <Rodape />
     </BrowserRouter>
